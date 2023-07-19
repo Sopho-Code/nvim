c286c8d9f16e17eeb79618f3ae3094d814870e9d
Eliphaz Bouye <eliphazbouye@gmail.com>
2023-07-19 16:32:39 +0000

    update

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -11,7 +11,7 @@ bind("n", "N", "Nzzzv")
 bind("n", "#", "#zz")
 bind("n", "*", "*zz")
 
--- Moving lines and preserving indentation 
+-- Moving lines and preserving indentation
 bind("n", "<C-j>", ":move .+1<CR>==")
 bind("n", "<C-k>", ":move .-2<CR>==")
 bind("v", "<C-j>", ":move '>+1<CR>gv=gv")
@@ -41,70 +41,72 @@ bind("n", "<leader>tc", ":tabclose<CR>")
 bind("n", "<leader>uh", "<cmd>set invhlsearch<CR>")
 
 -- Telescope mapping
-local builtin = require('telescope.builtin')
-bind('n', '<leader>ff', builtin.find_files, {})
-bind('n', '<leader>g', builtin.live_grep, {})
-bind('n', '<leader>b', builtin.buffers, {})
-bind('n', '<leader>ht', builtin.help_tags, {})
-bind('n', '<leader>o', builtin.oldfiles, {})
+local builtin = require("telescope.builtin")
+bind("n", "<leader>ff", builtin.find_files, {})
+bind("n", "<leader>g", builtin.live_grep, {})
+bind("n", "<leader>b", builtin.buffers, {})
+bind("n", "<leader>ht", builtin.help_tags, {})
+bind("n", "<leader>o", builtin.oldfiles, {})
 
 -- LSP MAPPINGS
 -- LSP SAGA
 bind("n", "gh", "<cmd>Lspsaga lsp_finder<CR>")
-bind({"n","v"}, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+bind({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
 bind("n", "gr", "<cmd>Lspsaga rename<CR>")
 bind("n", "gr", "<cmd>Lspsaga rename ++project<CR>")
-bind("n","gt", "<cmd>Lspsaga goto_type_definition<CR>")
+bind("n", "gt", "<cmd>Lspsaga goto_type_definition<CR>")
 bind("n", "<leader>sl", "<cmd>Lspsaga show_line_diagnostics<CR>")
 bind("n", "<leader>sc", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
 bind("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
 bind("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
 bind("n", "[E", function()
-  require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
+	require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
 end)
 bind("n", "]E", function()
-  require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
+	require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
 end)
-bind("n","<leader>to", "<cmd>Lspsaga outline<CR>")
+bind("n", "<leader>to", "<cmd>Lspsaga outline<CR>")
 bind("n", "K", "<cmd>Lspsaga hover_doc<CR>")
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-bind('n', '<leader>e', vim.diagnostic.open_float)
-bind('n', '[d', vim.diagnostic.goto_prev)
-bind('n', ']d', vim.diagnostic.goto_next)
-bind('n', '<leader>q', vim.diagnostic.setloclist)
-bind('n', '<leader>Q', vim.diagnostic.setqflist)
-
+bind("n", "<leader>e", vim.diagnostic.open_float)
+bind("n", "[d", vim.diagnostic.goto_prev)
+bind("n", "]d", vim.diagnostic.goto_next)
+bind("n", "<leader>q", vim.diagnostic.setloclist)
+bind("n", "<leader>Q", vim.diagnostic.setqflist)
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
-    vim.keymap.set('n', '<leader>so', require('telescope.builtin').lsp_references, attach_opts)
-  end,
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+		vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+		vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+		vim.keymap.set("n", "<space>wl", function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, opts)
+		vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "<space>f", function()
+			vim.lsp.buf.format({ async = true })
+		end, opts)
+		vim.keymap.set("n", "<leader>so", require("telescope.builtin").lsp_references, attach_opts)
+	end,
 })
+
+-- trouble
+bind("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,35 +1,99 @@
 return {
-  {
-      "neovim/nvim-lspconfig",
-      config = function()
-          -- Add additional capabilities supported by nvim-cmp
-          local capabilities = require("cmp_nvim_lsp").default_capabilities()
-          local lspconfig = require'lspconfig'
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			-- Add additional capabilities supported by nvim-cmp
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-          lspconfig.pyright.setup {
-              capabilities = capabilities,
-          }
-          lspconfig.tsserver.setup {
-              capabilities = capabilities,
-          }
-          lspconfig.docker_compose_language_service.setup{
-              capabilities = capabilities,
-          }
-          lspconfig.dockerls.setup{
-              capabilities = capabilities,
-          }
-         -- vim.diagnostic.config {
-         --   virtual_text = false,
-         --   update_in_insert = true,
-         -- }
-      end
-  },
-  {
-    "glepnir/lspsaga.nvim",
-    event = "LspAttach",
-    config = function()
-      require("lspsaga").setup({})
-    end,
-    dependencies = { {"nvim-tree/nvim-web-devicons"} }
-  }
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.docker_compose_language_service.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.dockerls.setup({
+				capabilities = capabilities,
+			})
+			vim.diagnostic.config({
+				virtual_text = false,
+				update_in_insert = true,
+			})
+
+			lspconfig.eslint.setup({})
+		end,
+	},
+	{
+		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+		config = function()
+			require("lsp_lines").setup({})
+		end,
+	},
+	{
+		"glepnir/lspsaga.nvim",
+		event = "LspAttach",
+		config = function()
+			require("lspsaga").setup({})
+		end,
+		dependencies = { { "nvim-tree/nvim-web-devicons" } },
+	},
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+			local formatting = null_ls.builtins.formatting
+			local diagnostics = null_ls.builtins.diagnostics
+
+			null_ls.setup({
+				sources = {
+					formatting.stylua,
+					formatting.prettier.with({
+						extra_args = { "--space-before-function-paren,--single-quote", "jsx-single-quote" },
+					}),
+					formatting.black.with({ extra_args = { "--fast" } }),
+					formatting.eslint,
+					-- diagnostics.eslint,
+					diagnostics.flake8,
+				},
+				on_attach = function(client, bufnr)
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								vim.lsp.buf.format()
+							end,
+						})
+					end
+				end,
+			})
+		end,
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			position = "bottom", -- position of the list can be: bottom, top, left, right
+			height = 10, -- height of the trouble list when position is top or bottom
+			width = 50, -- width of the list when position is left or right
+			icons = true, -- use devicons for filenames
+			fold_open = "", -- icon used for open folds
+			fold_closed = "", -- icon used for closed folds
+			signs = {
+				-- icons / text used for a diagnostic
+				error = "",
+				warning = "",
+				hint = "",
+				information = "",
+				other = "",
+			},
+			auto_open = false, -- automatically open the list when you have diagnostics
+			auto_close = true, -- automatically close the list when you have no diagnostics
+			use_diagnostic_signs = false,
+		},
+	},
 }
